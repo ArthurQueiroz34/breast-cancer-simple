5a8b7a2b25013ab48df37d074c39687e64b185b9
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-02-04 16:05:26 -0300

    Breast cancer cross-validation

---
+++ breast_cancer_cross/breast_cancer_cross.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+forecasters = pd.read_csv('entry_breast.csv')
+division = pd.read_csv('out_breast.csv')
+
+def createNetwork():
+    classifier = Sequential()
+    classifier.add(Dense(units = 16, activation = 'relu', 
+                     kernel_initializer= 'random_uniform', input_dim = 30))
+    classifier.add(Dense(units = 16, activation = 'relu', 
+                     kernel_initializer= 'random_uniform'))
+    classifier.add(Dense(units = 1, activation = 'sigmoid'))
+    
+    optimizer = keras.optimizers.Adam(lr = 0.001, decay = 0.0001, clipvalue = 0.5)
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', 
+                   metrics = ['binary_accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_fn = createNetwork,
+                             epochs = 100,
+                             batch_size = 10)
+results = cross_val_score(estimator = classifier,
+                          x = forecasters, y = division,
+                          cv = 10, scoring = 'accuracy')
+average = results.mean()
+bias = results.std()
