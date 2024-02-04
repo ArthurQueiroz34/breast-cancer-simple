c592b1a172a9936a7c6d309b90a7f212512313ca
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-02-04 12:35:22 -0300

    Neural network structure

--- breast_cancer_simple/breast_cancer_simple.py
+++ breast_cancer_simple/breast_cancer_simple.py
@@ -5,3 +5,11 @@ division = pd.read_csv('out_breast.csv')
 
 from sklearn.model_selection import train_test_split
 forecasters_training, forecasters_test, division_training, division_test = train_test_split(forecasters, division, test_size=0.25)
+
+import keras
+from keras.models import Sequential 
+from keras.layers import Dense
+classifier = Sequential()
+classifier.add(Dense(units = 16, activation = 'relu', 
+                     kernel_initializer= 'random_uniform', input_dim = 30))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
